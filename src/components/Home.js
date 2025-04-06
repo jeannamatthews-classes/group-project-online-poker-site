70972facf2e9ea9180b4b00be7c186642286b3ad
IWasKidnapped <sam.r.ongstad@gmail.com>
2025-04-06 13:03:17 -0400

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import {useNavigate } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+
+    const[username, setUsername] = useState('');
+    const navigate = useNavigate();
+
+    const handleJoinGame = () => { 
+        if(username.trim() !== ''){
+            localStorage.setItem('username', username);
+            navigate('/game');
+        }
+        else{
+            alert('Please Enter A Username Before Joining the Game');
+        }
+    }
+
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Poker Game</h1>
+      <div className="username-join-container">
+        <input
+            type="text"
+            className="username-input"
+            placeholder="Enter Your Username"
+            value={username}
+            onChange={(e)=> setUsername(e.target.value)}
+            />
+        <button className="join-button" onClick={handleJoinGame}>
+            Join Game
+        </button>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
